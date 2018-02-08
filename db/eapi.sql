8815a59842227133705c2f343799198c75180cfe
Domingo Perez <domingoperezguerrero@gmail.com>
2018-02-08 16:43:42 +0000

    Realaciones creadas entre los modelos

--- db/eapi.sql
+++ db/eapi.sql
@@ -12,6 +12,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- La exportación de datos fue deseleccionada.
+-- La exportación de datos fue deseleccionada.
+-- La exportación de datos fue deseleccionada.
+-- La exportación de datos fue deseleccionada.
+-- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
